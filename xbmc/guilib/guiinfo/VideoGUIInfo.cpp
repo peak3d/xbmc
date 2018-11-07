1638515a3ebcfe6defdd5aea57f69cdbcd6c1cbf
peak3d <pfau@peak3d.de>
2018-11-07 13:54:54 +0100

    multisel implementation

--- xbmc/guilib/guiinfo/VideoGUIInfo.cpp
+++ xbmc/guilib/guiinfo/VideoGUIInfo.cpp
@@ -22,6 +22,7 @@
 #include "guilib/StereoscopicsManager.h"
 #include "guilib/WindowIDs.h"
 #include "settings/AdvancedSettings.h"
+#include "settings/lib/Setting.h"
 #include "settings/Settings.h"
 #include "settings/SettingsComponent.h"
 #include "utils/StringUtils.h"
@@ -331,12 +332,14 @@ bool CVideoGUIInfo::GetLabel(std::string& value, const CFileItem *item, int cont
         break;
       case LISTITEM_PLOT:
         {
-          int iShowPlot = CServiceBroker::GetSettingsComponent()->GetSettings()->GetInt(CSettings::SETTING_VIDEOLIBRARY_SHOWUNWATCHEDPLOTS);
+          std::shared_ptr<CSettingList> setting(std::dynamic_pointer_cast<CSettingList>(
+            CServiceBroker::GetSettingsComponent()->GetSettings()->GetSetting(CSettings::SETTING_VIDEOLIBRARY_SHOWUNWATCHEDPLOTS)));
+
           if (tag->m_type != MediaTypeTvShow &&
               tag->m_type != MediaTypeVideoCollection &&
               tag->GetPlayCount() == 0 &&
-              ((tag->m_type == MediaTypeMovie && (iShowPlot == CSettings::VIDEOLIBRARY_PLOTS_SHOW_UNWATCHED_TVSHOWEPISODES || iShowPlot == CSettings::VIDEOLIBRARY_PLOTS_HIDE_ALL)) || // show plot for unwatched movies
-              (tag->m_type == MediaTypeEpisode && (iShowPlot == CSettings::VIDEOLIBRARY_PLOTS_SHOW_UNWATCHED_MOVIES || iShowPlot == CSettings::VIDEOLIBRARY_PLOTS_HIDE_ALL)))) // show plot for unwatched tv show episodes
+              ((tag->m_type == MediaTypeMovie && (setting && (setting->FindIntInList(CSettings::VIDEOLIBRARY_PLOTS_SHOW_UNWATCHED_TVSHOWEPISODES) || setting->FindIntInList(CSettings::VIDEOLIBRARY_PLOTS_HIDE_ALL)))) || // show plot for unwatched movies
+              (tag->m_type == MediaTypeEpisode && (setting && (setting->FindIntInList(CSettings::VIDEOLIBRARY_PLOTS_SHOW_UNWATCHED_MOVIES) || setting->FindIntInList(CSettings::VIDEOLIBRARY_PLOTS_HIDE_ALL)))))) // show plot for unwatched tv show episodes
           {
             value = g_localizeStrings.Get(20370);
           }
