68ff09056a7ac154cc2b4341643c55c4fcfe9440
Lukas Rusak <lorusak@gmail.com>
2019-06-03 13:34:38 -0700

    fixup! MemUtils: breakup into platform specific files

---
+++ xbmc/platform/android/MemUtils.cpp
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (C) 2005-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#include "utils/MemUtils.h"
+
+#include <stdlib.h>
+
+#undef ALIGN
+#define ALIGN(value, alignment) (((value)+(alignment-1))&~(alignment-1))
+
+namespace KODI
+{
+namespace MEMORY
+{
+
+// aligned memory allocation.
+// in order to do so - we alloc extra space and store the original allocation in it (so that we can free later on).
+// the returned address will be the nearest aligned address within the space allocated.
+void* AlignedMalloc(size_t s, size_t alignTo)
+{
+  if (CJNIBase::GetSDKVersion() >= 28)
+  {
+    return aligned_alloc(alignTo, s);
+  }
+  else
+  {
+    char *pFull = (char*)malloc(s + alignTo + sizeof(char *));
+    char *pAligned = (char *)ALIGN(((unsigned long)pFull + sizeof(char *)), alignTo);
+
+    *(char **)(pAligned - sizeof(char*)) = pFull;
+
+    return(pAligned);
+  }
+}
+
+void AlignedFree(void* p)
+{
+  if (!p)
+    return;
+
+  if (CJNIBase::GetSDKVersion() >= 28)
+  {
+    free(p);
+  }
+  else
+  {
+    char *pFull = *(char **)(((char *)p) - sizeof(char *));
+    free(pFull);
+  }
+}
+
+void GetMemoryStatus(MemoryStatus* buffer)
+{
+  if (!buffer)
+    return;
+}
+
+}
+}
